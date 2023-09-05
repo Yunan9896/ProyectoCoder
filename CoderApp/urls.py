d0f9ccf3dde67e03e7ea4e8b2743cea4c617f6fa
yunan <9896yunan@gmail.com>
2023-09-05 00:47:07 -0300

    pruebaVersion1

--- CoderApp/urls.py
+++ CoderApp/urls.py
@@ -6,9 +6,9 @@ from .views import *
 urlpatterns = [
     path('agregar-curso/<nombre>/<comision>', curso),
     path('lista-curso/', listar_curso),
-    path('', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
+    path('', inicio, name='inicio'),
+    path('cursos/', cursos, name='cursos'),
+    path('profesores/', profesores, name='profesores'),
+    path('estudiantes/', estudiantes, name='estudiantes'),
+    path('entregables/', entregables, name='entregables'),
 ]
